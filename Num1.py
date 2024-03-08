36a3689035bbefdfeec0eb5a1bf6fff211bd3c26
ArthurLVidal <arthurodri2017@gmail.com>
2024-03-08 19:21:42 -0300

    Questão 01 add

---
+++ Num1.py
@@ -0,0 +1,23 @@
+# 1) Observe o trecho de código abaixo:
+
+# int INDICE = 13, SOMA = 0, K = 0;
+INDICE = 13
+SOMA= 0
+K = 0
+# enquanto K < INDICE faça
+# {
+while K < INDICE:
+    # K = K + 1;
+    K += 1
+    # SOMA = SOMA + K;
+    SOMA += K
+
+# }
+# imprimir(SOMA);
+print(SOMA)
+
+# ==> SAÍDA
+# 91
+
+# Ao final do processamento, qual será o valor da variável SOMA?
+# RESP: A variavel SOMA terá o valor igual a 91
